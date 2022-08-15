71192d374b59753e4e44226d89a10b9fff5896f0
character_flat <88619728+character-flat@users.noreply.github.com>
2022-08-15 11:36:14 +0530

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
+$dbServername = "lin-7068-4824-mysql-primary.servers.linodedb.net";
+$dbUsername = "linroot";
+$dbPassword = "E6q8TcUJp4MO-czv";
 $dbName = "vaccportal";
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 if (!$conn) {
